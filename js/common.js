6df672ad518d5cd4c468f19829d2b5d53deada7d
Alexander Sotskii <64553081+Alexanderwebsot@users.noreply.github.com>
2022-02-19 23:21:23 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  
+
+  const swiper2 = new Swiper('.porfolio-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    speed: 600,
+    spaceBetween: 60,
+  });
+}
